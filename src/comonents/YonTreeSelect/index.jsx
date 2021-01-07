ff048bf292209bdfc25dfb79fdc509f98becf0f0
陈世杰 <237100956@qq.com>
2021-01-07 20:39:03 +0800

    补充 下拉树

--- src/comonents/YonTreeSelect/index.jsx
+++ src/comonents/YonTreeSelect/index.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import { TreeSelect } from 'antd'
 
 import './index.css'
+const { TreeNode, SHOW_ALL, SHOW_PARENT, SHOW_CHILD } = TreeSelect
+
 const YonTreeSelect = (props) => {
     return(
         <TreeSelect
           {...props}
           switcherIcon={<span></span>}
+          maxTagCount={'responsive'}
         />
     )
 }
+
+YonTreeSelect.TreeNode = TreeNode
+YonTreeSelect.SHOW_ALL = SHOW_ALL
+YonTreeSelect.SHOW_PARENT = SHOW_PARENT
+YonTreeSelect.SHOW_CHILD = SHOW_CHILD
+
 export default YonTreeSelect
--- src/stories/TreeSelect.stories.js
+++ src/stories/TreeSelect.stories.js
@@ -1,44 +1,130 @@
-import React, { useState } from "react";
-import { storiesOf } from "@storybook/react";
-import YonTreeSelect from "../comonents/YonTreeSelect";
+import React, { useState } from "react"
+import { storiesOf } from "@storybook/react"
+import YonTreeSelect from "../comonents/YonTreeSelect"
+
+const SHOW_ALL = YonTreeSelect.SHOW_ALL
+const TreeNode = YonTreeSelect.TreeNode
 
 const TreeSelect = () => {
-  const [selectVal, changeVal] = useState("");
+  const [selectVal, changeVal] = useState("")
+  const [checkVals, setCheckVals] = useState([])
+  const [selectVals, setSelectVals] = useState(undefined)
   const treeData = [
     {
       title: "Node1",
       value: "0-0",
+      key: "0-0",
       children: [
         {
           title: "Child Node1",
-          value: "0-0-1",
-        },
-        {
-          title: "Child Node2",
-          value: "0-0-2",
+          value: "0-0-0",
+          key: "0-0-0",
         },
       ],
     },
     {
       title: "Node2",
       value: "0-1",
+      key: "0-1",
+      children: [
+        {
+          title: "Child Node3",
+          value: "0-1-0",
+          key: "0-1-0",
+        },
+        {
+          title: "Child Node4",
+          value: "0-1-1",
+          key: "0-1-1",
+        },
+        {
+          title: "Child Node5",
+          value: "0-1-2",
+          key: "0-1-2",
+        },
+      ],
     },
-  ];
-  return (
-    <div style={{ width: 250 }}>
-      <YonTreeSelect
-        style={{ width: "100%" }}
-        value={selectVal}
-        dropdownStyle={{ maxHeight: 400, overflow: "auto" }}
-        treeData={treeData}
-        placeholder="Please select"
-        treeDefaultExpandAll
-        onChange={(val) => {
-          changeVal(val);
-        }}
-      />
-    </div>
-  );
-};
+  ]
 
-storiesOf("Select", module).add("treeSelect", TreeSelect);
+  return (
+    <>
+      <div style={{ margin: "0 0 50px 0" }}>
+        <h3>普通下拉树</h3>
+        <div style={{ width: 250 }}>
+          <YonTreeSelect
+            style={{ width: "100%" }}
+            value={selectVal}
+            dropdownStyle={{ maxHeight: 400, overflow: "auto" }}
+            treeData={treeData}
+            placeholder="Please select"
+            treeDefaultExpandAll
+            onChange={(val) => {
+              changeVal(val)
+            }}
+          />
+        </div>
+      </div>
+      <div style={{ margin: "0 0 50px 0" }}>
+        <h3>下拉多选树</h3>
+        <div style={{ width: 250 }}>
+          <YonTreeSelect
+            showSearch
+            style={{ width: "100%" }}
+            value={selectVals}
+            dropdownStyle={{ maxHeight: 400, overflow: "auto" }}
+            placeholder="Please select"
+            allowClear
+            multiple
+            treeDefaultExpandAll
+            onChange={(val) => {
+              setSelectVals(val)
+            }}
+          >
+            <TreeNode value="parent 1" title="parent 1">
+              <TreeNode value="parent 1-0" title="parent 1-0">
+                <TreeNode value="leaf1" title="my leaf" />
+                <TreeNode value="leaf2" title="your leaf" />
+              </TreeNode>
+              <TreeNode value="parent 1-1" title="parent 1-1">
+                <TreeNode
+                  value="sss"
+                  title={<b style={{ color: "#08c" }}>sss</b>}
+                />
+              </TreeNode>
+            </TreeNode>
+          </YonTreeSelect>
+        </div>
+      </div>
+      <div style={{ margin: "0 0 50px 0" }}>
+        <h3>可勾选下拉多选树</h3>
+        <div style={{ width: 250 }}>
+          <YonTreeSelect
+            style={{ width: "100%" }}
+            value={checkVals}
+            dropdownStyle={{ maxHeight: 400, overflow: "auto" }}
+            treeData={treeData}
+            placeholder="Please select"
+            treeDefaultExpandAll
+            showCheckedStrategy={SHOW_ALL}
+            treeCheckable={true}
+            onChange={(val) => {
+              setCheckVals(val)
+            }}
+          />
+        </div>
+      </div>
+    </>
+  )
+}
+
+const note = `
+  ### 注释
+  只修改了样式，属性全部继承 antd。
+  注意以下两点：
+  * switcherIcon 已重置为空的span标签，展开收起图标已在css中重写；
+  * maxTagCount 最多展示tag数已改为响应式
+`
+
+storiesOf("TreeSelect", module).add("treeSelect", TreeSelect, {
+  info: { text: note },
+})
