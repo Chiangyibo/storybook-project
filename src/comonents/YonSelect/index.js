4a2c180555cc681a3156ec5b6e235d12796d3e6c
蒋一博 <jiangybo@yonyou.com>
2021-01-08 17:15:31 +0800

    select 新增 label、required API

--- src/comonents/YonSelect/index.js
+++ src/comonents/YonSelect/index.js
@@ -1,18 +1,64 @@
 
 import React from 'react';
 import { Select } from 'antd';
+
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 import './index.css';
+
+const lang = {
+    "zh_CN":{
+        "RPA_L_90000000": "必输项",
+    },
+    "zh_TW":{
+        "RPA_L_90000000": "必輸項",
+    },
+    "en_US":{
+        "RPA_L_90000000": "Required",
+    },
+}
+
+let locale = window.localStorage.getItem('locale') || 'zh_CN';
+
+
 
 const { Option } = Select;
 
 const YonSelect = (props) => {
-    return (
+    return (<span className="clearfix" style={{display:'inline-block',height:'44px'}}>
+    {
+        !!props.label && <span className="clearfix fl" style={{ display:'inline-block', width:'126px' }} >
+            <span className={classNames({"required-star": props.required}, 'fr', 'yon-label-style')}>
+                {props.label}
+            </span>
+        </span>
+    }
+    
+    <span className="fl" style={{position:'relative'}}>
         <Select {...props}>
             {props.children}
         </Select>
+        <em style={{
+            position:'absolute',
+            left:'0',
+            top:'28px',
+            fontSize:'12px',
+            lineHeight:'12px',
+            fontStyle:'normal',
+            color: '#ccc',
+            visibility: props.required ? 'visible' : 'hidden'
+        }}>
+            {lang[locale].RPA_L_90000000}
+        </em>
+    </span>
+</span>
+        
     );
 };
-
+YonSelect.propTypes = {
+    label: PropTypes.string,
+    required: PropTypes.bool
+};
 YonSelect.Option = Option;
 
 
--- src/stories/Select.stories.js
+++ src/stories/Select.stories.js
@@ -5,16 +5,28 @@ import YonSelect from '../comonents/YonSelect';
 const { Option } = YonSelect;
 
 const Select = () => <>
-    <YonSelect defaultValue="2">
-        <Option key="1" value="1">选项1</Option>
-        <Option key="2" value="2">选项2</Option>
-        <Option key="3" value="3">选项3</Option>
-    </YonSelect>
+        <YonSelect defaultValue="2" >
+          <Option key="1" value="1">选项1</Option>
+          <Option key="2" value="2">选项2</Option>
+          <Option key="3" value="3">选项3</Option>
+        </YonSelect>
+        <YonSelect defaultValue="2" label="机器人名称">
+          <Option key="1" value="1">选项1</Option>
+          <Option key="2" value="2">选项2</Option>
+          <Option key="3" value="3">选项3</Option>
+        </YonSelect>
+        <YonSelect label="机器人名称" required style={{ width: 200 }}>
+          <Option key="1" value="1">选项1</Option>
+          <Option key="2" value="2">选项2</Option>
+          <Option key="3" value="3">选项3</Option>
+        </YonSelect>
 </>;
 
 const note = `
   ### 注释
-  仅改了样式，其他 API 全部继承 antd。
+  仅改了样式，添加了2个 API: label、required, 其他 API 全部继承 antd。
+  * label 输入框前的标题
+  * required 是否为必输项
 `
 
 storiesOf("Select", module).add('Select', Select, {info:{text: note}});
